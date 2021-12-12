7803c9d0e5142595890bc2efb050eaae9fa37c42
Ross <xkjq@ymail.com>
2021-12-12 18:06:13 +0000

    .

--- js/main.js
+++ js/main.js
@@ -142,23 +142,22 @@ async function retrievePacketList() {
       error: function(httpObj, textStatus) {
         if (httpObj.status == 401 || httpObj.status == 404) {
           $.notify("Unable to login", "error");
-
         }
       },
     })
     .done(function() {})
-    .fail(function() {
-      $.getJSON("packets", function(data) {
-        if (data.hasOwnProperty("exams")) {
-          loadExamList(data);
-        } else {
-          loadPacketList(data);
-        }
-      }).fail(function(jqXHR, textStatus, errorThrown) {
-        console.log("No packet list available");
-        showLoginDialog();
-      });
-    })
+    //.fail(function() {
+    //  $.getJSON("packets", function(data) {
+    //    if (data.hasOwnProperty("exams")) {
+    //      loadExamList(data);
+    //    } else {
+    //      loadPacketList(data);
+    //    }
+    //  }).fail(function(jqXHR, textStatus, errorThrown) {
+    //    console.log("No packet list available");
+    //    showLoginDialog();
+    //  });
+    //})
     .always(function() {});
 }
 
